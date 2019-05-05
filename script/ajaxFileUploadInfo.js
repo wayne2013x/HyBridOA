53da98b714640a0a1439118eecaf3d4e69a7cced
wayne2013x <qiuweibine@163.com>
2019-05-05 21:44:49 +0800

    添加项目

---
+++ script/ajaxFileUploadInfo.js
@@ -0,0 +1,180 @@
+﻿
+jQuery.extend({
+
+
+    createUploadIframe: function (id, uri) {
+        //create frame
+        var frameId = 'jUploadFrame' + id;
+        var iframeHtml = '<iframe id="' + frameId + '" name="' + frameId + '" style="position:absolute; top:-9999px; left:-9999px"';
+        if (window.ActiveXObject) {
+            if (typeof uri == 'boolean') {
+                iframeHtml += ' src="' + 'javascript:false' + '"';
+
+            }
+            else if (typeof uri == 'string') {
+                iframeHtml += ' src="' + uri + '"';
+
+            }
+        }
+        iframeHtml += ' />';
+        JQ(iframeHtml).appendTo(document.body);
+
+        return JQ('#' + frameId).get(0);
+    },
+    createUploadForm: function (id, fileElementId, data) {
+        //create form	
+        var formId = 'jUploadForm' + id;
+        var fileId = 'jUploadFile' + id;
+        var form = JQ('<form  action="" method="POST" name="' + formId + '" id="' + formId + '" enctype="multipart/form-data"></form>');
+        if (data) {
+            for (var i in data) {
+                JQ('<input type="hidden" name="' + i + '" value="' + data[i] + '" />').appendTo(form);
+            }
+        }
+        var oldElement = JQ('#' + fileElementId);
+        var newElement = JQ(oldElement).clone();
+        JQ(oldElement).attr('id', fileId);
+        JQ(oldElement).before(newElement);
+        JQ(oldElement).appendTo(form);
+
+
+
+        //set attributes
+        JQ(form).css('position', 'absolute');
+        JQ(form).css('top', '-1200px');
+        JQ(form).css('left', '-1200px');
+        JQ(form).appendTo('body');
+        return form;
+    },
+
+    ajaxFileUpload: function (s) {
+        // TODO introduce global settings, allowing the client to modify them for all requests, not only timeout		
+        s = JQ.extend({}, JQ.ajaxSettings, s);
+        var id = new Date().getTime()
+        var form = JQ.createUploadForm(id, s.fileElementId, (typeof (s.data) == 'undefined' ? false : s.data));
+        var io = JQ.createUploadIframe(id, s.secureuri);
+        var frameId = 'jUploadFrame' + id;
+        var formId = 'jUploadForm' + id;
+        // Watch for a new set of requests
+        if (s.global && !JQ.active++) {
+            JQ.event.trigger("ajaxStart");
+        }
+        var requestDone = false;
+        // Create the request object
+        var xml = {}
+        if (s.global)
+            JQ.event.trigger("ajaxSend", [xml, s]);
+        // Wait for a response to come back
+        var uploadCallback = function (isTimeout) {
+            var io = document.getElementById(frameId);
+            try {
+                if (io.contentWindow) {
+                    xml.responseText = io.contentWindow.document.body ? io.contentWindow.document.body.innerHTML : null;
+                    xml.responseXML = io.contentWindow.document.XMLDocument ? io.contentWindow.document.XMLDocument : io.contentWindow.document;
+
+                } else if (io.contentDocument) {
+                    xml.responseText = io.contentDocument.document.body ? io.contentDocument.document.body.innerHTML : null;
+                    xml.responseXML = io.contentDocument.document.XMLDocument ? io.contentDocument.document.XMLDocument : io.contentDocument.document;
+                }
+            } catch (e) {
+                JQ.handleError(s, xml, null, e);
+            }
+            if (xml || isTimeout == "timeout") {
+                requestDone = true;
+                var status;
+                try {
+                    status = isTimeout != "timeout" ? "success" : "error";
+                    // Make sure that the request was successful or notmodified
+                    if (status != "error") {
+                        // process the data (runs the xml through httpData regardless of callback)
+                        var data = JQ.uploadHttpData(xml, s.dataType);
+                        // If a local callback was specified, fire it and pass it the data
+                        if (s.success)
+                            s.success(data, status);
+
+                        // Fire the global callback
+                        if (s.global)
+                            JQ.event.trigger("ajaxSuccess", [xml, s]);
+                    } else
+                        JQ.handleError(s, xml, status);
+                } catch (e) {
+                    status = "error";
+                    JQ.handleError(s, xml, status, e);
+                }
+
+                // The request was completed
+                if (s.global)
+                    JQ.event.trigger("ajaxComplete", [xml, s]);
+
+                // Handle the global AJAX counter
+                if (s.global && ! --JQ.active)
+                    JQ.event.trigger("ajaxStop");
+
+                // Process result
+                if (s.complete)
+                    s.complete(xml, status);
+
+                JQ(io).unbind()
+
+                setTimeout(function () {
+                    try {
+                        JQ(io).remove();
+                        JQ(form).remove();
+
+                    } catch (e) {
+                        JQ.handleError(s, xml, null, e);
+                    }
+
+                }, 100)
+
+                xml = null
+
+            }
+        }
+        // Timeout checker
+        if (s.timeout > 0) {
+            setTimeout(function () {
+                // Check to see if the request is still happening
+                if (!requestDone) uploadCallback("timeout");
+            }, s.timeout);
+        }
+        try {
+
+            var form = JQ('#' + formId);
+            JQ(form).attr('action', s.url);
+            JQ(form).attr('method', 'POST');
+            JQ(form).attr('target', frameId);
+            if (form.encoding) {
+                JQ(form).attr('encoding', 'multipart/form-data');
+            }
+            else {
+                JQ(form).attr('enctype', 'multipart/form-data');
+            }
+            JQ(form).submit();
+
+        } catch (e) {
+            JQ.handleError(s, xml, null, e);
+        }
+
+        JQ('#' + frameId).load(uploadCallback);
+        return { abort: function () { } };
+
+    },
+
+    uploadHttpData: function (r, type) {
+        var data = !type;
+        data = type == "xml" || data ? r.responseXML : r.responseText;
+        // If the type is "script", eval it in global context
+        if (type == "script")
+            JQ.globalEval(data);
+        // Get the JavaScript object, if JSON is used.
+        if (type == "json")
+            eval("data = " + data);
+        // evaluate scripts within html
+        if (type == "html")
+            JQ("<div>").html(data).evalScripts();
+
+        return data;
+    }
+})
+
